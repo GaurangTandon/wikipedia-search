88c887d17e2f6790552e9d8aef0c9bda0b1c1218
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2020-08-17 00:19:45 +0530

    Parser works on mvce file

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -3,46 +3,92 @@
 #include<iostream>
 #include<fstream>
 
-class position {
-public:
-    double lat_, lon_;
+/*
+ * next_expect(): if `content` argument is set, then event type must be start element
+ */
 
-    position(xml::parser &p) {
-        p.next_expect(xml::parser::start_element, "position", xml::content::empty);
-        lat_ = p.attribute<double>("lat");
-        lon_ = p.attribute<double>("lon");
-        std::cout << "Parsed position with " << lat_ << " " << lon_ << std::endl;
+class WikiPage {
+public:
+    std::string title, text;
+
+    WikiPage(xml::parser &p) {
+        p.next_expect(xml::parser::start_element, "page");
+
+        bool inTitle = false, inText = false;
+
+        for (auto e : p) {
+            switch (e) {
+               case xml::parser::start_element:
+                    if (p.name() == "title") {
+                        inTitle = true;
+                    }
+                    if (p.name() == "title") {
+                        inText = true;
+                    }
+                    break;
+                case xml::parser::end_element:
+                    if (p.name() == "title") {
+                        inTitle = false;
+                    }
+                    if (p.name() == "title") {
+                        inText = false;
+                    }
+                    if (p.name() == "page") {
+                        std::cout << title << " " << text << std::endl;
+                        return;
+                    }
+                    break;
+                case xml::parser::characters:
+                    if (inTitle) {
+                        title += p.value();
+                    }
+                    if (inText) {
+                        text += p.value();
+                    }
+                    break;
+                case xml::parser::start_attribute:
+                case xml::parser::end_attribute:
+                    break;
+            }
+        }
     }
 };
 
-class object {
+class WikiSiteInfo {
 public:
-    std::string name, type;
-    int id;
-
-    object(xml::parser &p) {
-        p.next_expect(xml::parser::start_element, "object", xml::content::complex);
-
-        id = p.attribute<int>("id");
-        name = p.attribute("name");
-        type = p.attribute("type");
-
-        std::cout << "Parsed " << id << " " << name << " " << type << std::endl;
+    WikiSiteInfo (xml::parser &p) {
+        p.next_expect(xml::parser::start_element, "siteinfo", xml::content::complex);
 
         while (p.peek() == xml::parser::start_element) {
-            new position(p);
+            p.next_expect(xml::parser::start_element);
+            p.next_expect(xml::parser::characters);
+            p.next_expect(xml::parser::end_element);
         }
 
-        p.next_expect(xml::parser::end_element);
+        p.next_expect(xml::parser::end_element, "siteinfo");
     }
 };
 
-const std::string filePath = "text.xml";
+class WikiObject {
+public:
+    WikiObject(xml::parser &p) {
+        p.next_expect(xml::parser::start_element, "mediawiki", xml::content::complex);
+
+        new WikiSiteInfo(p);
+
+        while (p.peek() == xml::parser::start_element) new WikiPage(p);
+
+        p.next_expect(xml::parser::end_element, "mediawiki");
+    }
+};
+
+const std::string filePath = "small.xml";
+
 int main() {
     try {
         std::ifstream ifs(filePath);
         xml::parser p(ifs, filePath);
-        new object(p);
+        new WikiObject(p);
     } catch (xml::parsing &e) {
         std::cout << e.what() << std::endl;
         return 1;
