40e831aef9270e961e53c6b0cf590b56755508dc
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2020-08-17 12:41:01 +0530

    Added namespace handling, got first half to work

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -7,12 +7,14 @@
  * next_expect(): if `content` argument is set, then event type must be start element
  */
 
+const std::string NS = "http://www.mediawiki.org/xml/export-0.10/";
+
 class WikiPage {
 public:
     std::string title, text;
 
     WikiPage(xml::parser &p) {
-        p.next_expect(xml::parser::start_element, "page");
+        p.next_expect(xml::parser::start_element, NS, "page");
 
         bool inTitle = false, inText = false;
 
@@ -30,7 +32,7 @@ public:
                     if (p.name() == "title") {
                         inTitle = false;
                     }
-                    if (p.name() == "title") {
+                    if (p.name() == "text") {
                         inText = false;
                     }
                     if (p.name() == "page") {
@@ -46,52 +48,59 @@ public:
                         text += p.value();
                     }
                     break;
-                case xml::parser::start_attribute:
-                case xml::parser::end_attribute:
+                default:
                     break;
             }
         }
     }
 };
 
+#define DEBUG std::cout << p.next() << " " << p.value() << " " << p.name() << " " << p.qname() << std::endl;
+
 class WikiSiteInfo {
 public:
     WikiSiteInfo (xml::parser &p) {
-        p.next_expect(xml::parser::start_element, "siteinfo", xml::content::complex);
+        p.next_expect(xml::parser::start_element, NS, "siteinfo");
+        p.attribute_map();
 
-        while (p.peek() == xml::parser::start_element) {
-            p.next_expect(xml::parser::start_element);
-            p.next_expect(xml::parser::characters);
-            p.next_expect(xml::parser::end_element);
+        for (auto e : p) {
+            if (e == xml::parser::end_element) {
+                if (p.name() == "siteinfo") return;
+            }
         }
 
-        p.next_expect(xml::parser::end_element, "siteinfo");
+        assert(false);
     }
 };
+
 
 class WikiObject {
 public:
     WikiObject(xml::parser &p) {
-        p.next_expect(xml::parser::start_element, "mediawiki", xml::content::complex);
+        p.next_expect(xml::parser::start_element, NS, "mediawiki", xml::content::complex);
+        p.attribute_map();
 
         new WikiSiteInfo(p);
 
         while (p.peek() == xml::parser::start_element) new WikiPage(p);
 
-        p.next_expect(xml::parser::end_element, "mediawiki");
+        p.next_expect(xml::parser::end_element, NS, "mediawiki");
     }
 };
 
-const std::string filePath = "small.xml";
+const std::string filePath = "mvce.xml";
 
 int main() {
-    try {
+//    try {
         std::ifstream ifs(filePath);
+        // our xml is in the namespace denoted by the xmlns attribute in the XML file
         xml::parser p(ifs, filePath);
+        // if you put the receive_namespace_decls flag in the parser argument, it will start receiving
+        // namespace decls also, which may be desirable, but for now, skip it and hardcode the namespace
         new WikiObject(p);
-    } catch (xml::parsing &e) {
-        std::cout << e.what() << std::endl;
-        return 1;
-    }
+//    } catch (xml::parsing &e) {
+//        std::cout << e.what() << std::endl;
+//        return 1;
+//    }
     return 0;
 }
