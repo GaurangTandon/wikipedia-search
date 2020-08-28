91a290f437fdc77da46e96ccfef517a5ef3d3a58
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2020-08-28 22:22:11 +0530

    Add basic search stuff

--- search/search.cpp
+++ search/search.cpp
@@ -1,14 +1,12 @@
 #include <cassert>
 #include <string>
 #include <vector>
-#include <iostream>
-#include "../common.h"
+#include "../preprocess/preprocess.cpp"
 
-int main(int argc, char *argv[]) {
-    assert(argc == 2);
+Preprocessor processor;
+std::string outputDir;
 
-    char* query = argv[1];
-
+std::vector<std::string> extractZonalQueries(char *query) {
     std::vector<int> zones(255, -1);
     zones['t'] = TITLE_ZONE;
     zones['i'] = INFOBOX_ZONE;
@@ -19,7 +17,7 @@ int main(int argc, char *argv[]) {
     std::vector<std::string> zonalQueries(ZONE_COUNT);
     int currZone = TEXT_ZONE;
 
-    while (*query){
+    while (*query) {
         auto c = *query;
 
         if (zones[c] > -1 and *(query + 1) == QUERY_SEP) {
@@ -32,7 +30,46 @@ int main(int argc, char *argv[]) {
         query++;
     }
 
-    for (auto &string : zonalQueries) std::cout << string << std::endl;
+    return zonalQueries;
+}
+
+std::vector<std::vector<std::string>> searchResults(ZONE_COUNT);
+
+void performSearch(const std::string &query, int zone) {
+    auto tokens = processor.getStemmedTokens(query, 0, query.size() - 1);
+
+    for (auto &token : tokens) {
+
+    }
+}
+
+int main(int argc, char *argv[]) {
+    assert(argc == 3);
+    processor = Preprocessor();
+    outputDir = std::string(argv[1]);
+
+    int fileCount;
+    std::ifstream fileStats("file_stats.txt", std::ios_base::in);
+    fileStats >> fileCount;
+    assert(fileCount < 30);
+
+    char *query = argv[2];
+    auto zonalQueries = extractZonalQueries(query);
+    int zoneI = 0;
+
+    for (auto &zone : zonalQueries) {
+        performSearch(zone, zoneI);
+        zoneI++;
+    }
+
+    for (auto &result : searchResults) {
+        for (auto &title : result) {
+            std::cout << title << "; ";
+        }
+        std::cout << std::endl;
+        std::cout << "------";
+        std::cout << std::endl;
+    }
 
     return 0;
 }
